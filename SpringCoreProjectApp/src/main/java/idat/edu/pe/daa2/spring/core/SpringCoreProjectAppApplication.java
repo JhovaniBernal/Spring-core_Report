fbda46ff3656d1febc684ae4421459c471f4323d
Administrador <Administrador@DESKTOP-5E85M15>
2019-11-02 20:27:52 -0500

    finalizado

--- SpringCoreProjectApp/src/main/java/idat/edu/pe/daa2/spring/core/SpringCoreProjectAppApplication.java
+++ SpringCoreProjectApp/src/main/java/idat/edu/pe/daa2/spring/core/SpringCoreProjectAppApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import idat.edu.pe.daa2.spring.core.di.tradicional.Automovil;
+import idat.edu.pe.daa2.spring.core.di.tradicional.ViajarVehiculo;
 import idat.edu.pe.daa2.spring.core.ioc.SaludoEspannol;
 import idat.edu.pe.daa2.spring.core.ioc.SaludoI;
 import idat.edu.pe.daa2.spring.core.ioc.SaludoIngles;
@@ -15,10 +17,13 @@ public class SpringCoreProjectAppApplication {
 	public static void main(String[] args) {
 		
 		
-		String nombre = "Christian Castillo";
+		//String nombre = "Christian Castillo";
 		
 		ConfigurableApplicationContext cxt = SpringApplication.run(SpringCoreProjectAppApplication.class, args);
 		
+		
+		
+		/*
 		SaludoI saludo1 = (SaludoEspannol)cxt.getBean("saludoEspannol");
 		System.out.println("saludo 1 en español : " + saludo1.saludar(nombre));		
 				
@@ -26,7 +31,14 @@ public class SpringCoreProjectAppApplication {
 	    System.out.println("saludo 2 en ingles : " + saludo2.saludar(nombre));		
 	    
 	    SaludoI saludo3 = (SaludoPortugues)cxt.getBean("saludoBrasilero");
-	    System.out.println("saludo 3 en portugues : " + saludo3.saludar(nombre));		
+	    System.out.println("saludo 3 en portugues : " + saludo3.saludar(nombre));		   
+	    */	
+		
+         ViajarVehiculo viaje = new ViajarVehiculo();
+         
+         viaje.setVehiculo(cxt.getBean(Automovil.class));
+         viaje.empezarAcelerar();
+         
 	}
 
 }
